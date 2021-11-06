e974b505ebf483dfebca148c3509dc3b2078066f
evgen32br <4ibo32@mail.ru>
2021-11-06 15:01:18 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,10 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item dayTime = new Item();
+        Item entity = new Item(22, "Захар");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         String outDayAndTime = dayTime.getCreated().format(formatter);
         System.out.println(outDayAndTime);
+        System.out.println(entity);
     }
 }
