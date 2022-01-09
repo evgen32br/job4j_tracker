1a25bb29d330bf2409dc113a4a35469fe7a63401
evgen32br <4ibo32@mail.ru>
2022-01-09 16:14:53 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void noAdd() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenTwo = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertFalse(office.add(citizenTwo));
+    }
+}
