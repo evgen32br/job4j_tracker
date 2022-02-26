ba1d833d1cd1ce20fe67d3470a0e6006bce7c951
evgen32br <4ibo32@mail.ru>
2022-02-26 16:14:42 +0300

    2. Список адресов [#110062]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.*;
+public class ProfilesTest {
+
+    @Test
+    public void whenTestMethodCollect() {
+        Address first = new Address("City1", "Street1", 1, 1);
+        Address second = new Address("City2", "Street2", 2, 2);
+        Address third = new Address("City3", "Street3", 3, 3);
+        List<Profile> profiles = Arrays.asList(
+                new Profile(first),
+                new Profile(second),
+                new Profile(third)
+        );
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = Arrays.asList(first, second, third);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenTestCollect() {
+        Address orel = new Address("Орел", "Орловская", 56, 38);
+        Address bryansk = new Address("Брянск", "Авиационная", 20, 2);
+        Address tula = new Address("Тула", "Центральная", 10, 18);
+        List<Profile> profiles = Arrays.asList(
+                new Profile(orel),
+                new Profile(bryansk),
+                new Profile(tula)
+        );
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = Arrays.asList(bryansk, orel, tula);
+        assertEquals(expected, result);
+    }
+}
