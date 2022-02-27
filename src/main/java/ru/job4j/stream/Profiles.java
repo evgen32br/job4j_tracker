16a81cac0ebc945c62c969332e18338e35fcc997
evgen32br <4ibo32@mail.ru>
2022-02-27 22:02:11 +0300

    2. Список адресов [#110062 #269731]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,10 +6,9 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().
-                map(Profile::getAddress).
-                sorted(Comparator.comparing(Address::getCity)).
-                collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
