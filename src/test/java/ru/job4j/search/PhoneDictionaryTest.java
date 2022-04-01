bbf1f6b3c26284a5ba1daf3cf8964109c910c352
evgen32br <4ibo32@mail.ru>
2022-04-01 20:15:57 +0300

    2. Local-Variable Type Inference [#70626 #280546]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -9,21 +9,21 @@ public class PhoneDictionaryTest {
 
     @Test
     public void whenFind() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
     @Test
     public void whenFindNot() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Петр");
+        var persons = phones.find("Петр");
         assertTrue(persons.isEmpty());
     }
 }
